a9a6d115823292d0ad4b9c41b244bd209bfc3782
Keefer Taylor <keefer@keefertaylor.com>
2021-05-25 10:55:58 -0400

    actual params

--- deploy/src/deploy-testnet.ts
+++ deploy/src/deploy-testnet.ts
@@ -8,42 +8,42 @@ const params: DeployParams = {
   vestingContracts: [
     {
       owner: 'tz1YfB2H1NoZVUq4heHqrVX4oVp99yz8gwNq',
-      amountPerBlock: scaleTokenAmount(new BigNumber('2')),
-      vestingStartBlock: new BigNumber('170800'),
-      amount: scaleTokenAmount(new BigNumber('10000')),
+      amountPerBlock: new BigNumber('0.08561643835'),
+      vestingStartBlock: new BigNumber('210700'),
+      amount: scaleTokenAmount(new BigNumber('90000')),
     },
     {
       owner: 'tz1QM9J63wfTrGW7HLotr7Kshy8q8LhdorJe',
-      amountPerBlock: scaleTokenAmount(new BigNumber('3')),
-      vestingStartBlock: new BigNumber('170900'),
-      amount: scaleTokenAmount(new BigNumber('10000')),
+      amountPerBlock: new BigNumber('0.08561643835'),
+      vestingStartBlock: new BigNumber('210700'),
+      amount: scaleTokenAmount(new BigNumber('90000')),
     },
     {
       owner: 'tz1YvwHP7TDbAMWf2sFZ5AzjRPe4GyA7Fc46',
-      amountPerBlock: scaleTokenAmount(new BigNumber('4')),
-      vestingStartBlock: new BigNumber('171000'),
-      amount: scaleTokenAmount(new BigNumber('10000')),
+      amountPerBlock: new BigNumber('0.01902587519'),
+      vestingStartBlock: new BigNumber('210700'),
+      amount: scaleTokenAmount(new BigNumber('20000')),
     },
   ],
 
-  faucetAmount: scaleTokenAmount(new BigNumber('100000')),
-  maxFaucetDripSize: scaleTokenAmount(new BigNumber('100')),
+  faucetAmount: scaleTokenAmount(new BigNumber('0')),
+  maxFaucetDripSize: scaleTokenAmount(new BigNumber('0')),
 
-  airdropAmount: scaleTokenAmount(new BigNumber('200000')),
-  airdropAddress: 'tz1KoLibimdjUSfhrSpXwx4FhhhCq1JM5Etk',
+  airdropAmount: scaleTokenAmount(new BigNumber('150000')),
+  airdropAddress: 'tz1hoverof3f2F8NAavUyTjbFBstZXTqnUMS',
 
-  escrowAmount: scaleTokenAmount(new BigNumber('100')),
+  escrowAmount: scaleTokenAmount(new BigNumber('10000')),
   voteDelayBlocks: new BigNumber('1'),
-  voteLengthBlocks: new BigNumber('480'),
+  voteLengthBlocks: new BigNumber('20160'),
 
   minYayVotesPercentForEscrowReturn: new BigNumber('20'),
 
-  blocksInTimelockForExecution: new BigNumber('120'),
-  blocksInTimelockForCancellation: new BigNumber('240'),
+  blocksInTimelockForExecution: new BigNumber('8640'),
+  blocksInTimelockForCancellation: new BigNumber('14400'),
 
-  percentageForSuperMajority: new BigNumber('80'),
+  percentageForSuperMajority: new BigNumber('65'),
 
-  quorum: scaleTokenAmount(new BigNumber('2000')),
+  quorum: scaleTokenAmount(new BigNumber('200000')),
   upperQuorumCap: scaleTokenAmount(new BigNumber('900000')),
   lowerQuorumCap: scaleTokenAmount(new BigNumber('10000')),
 
